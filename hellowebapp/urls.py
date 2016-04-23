b45502bf931bfe858b9e60715c79c64a081ecade
mmlumba <mmlumba@gmail.com>
2016-04-23 13:06:25 -0400

    first commit to hellowebapp

---
+++ hellowebapp/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from collection import views
+
+urlpatterns = [
+ url(r'^$', views.index, name='home'),
+ url(r'^admin/', include(admin.site.urls)),
+]
